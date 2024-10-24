37998f564c6c5310c4b11221ec57751d270c438b
Angeli04 <113207808+Angeli04@users.noreply.github.com>
2024-10-24 19:39:18 -0300

    primer commit
    base del proyecto

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+// Configuración de Sequelize
+const sequelize = new Sequelize('agenda_consultorio', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+module.exports = sequelize;
