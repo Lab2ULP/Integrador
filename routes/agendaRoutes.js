76e5d9c882f9c941290242c6cf798872845eff1f
Salvador Torres <90728148+Salva080@users.noreply.github.com>
2024-11-12 16:02:01 -0300

    Falta que ande el submit Actualizar Turno

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const agendaController = require('../controllers/agendaController');
 
 router.get('/lista/agendas',agendaController.listarAgendas)
+router.post('/lista/agendas/:ID',agendaController.actualizarTurnos)
 router.get('/crear',agendaController.renderCrearAgenda)
 router.post('/crear',agendaController.crearAgenda)
 router.get('/agenda/:ID/:Pid',agendaController.listarTurnos)
