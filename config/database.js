29130562b4e3f588da12500281745bfe80b72245
Angeli04 <113207808+Angeli04@users.noreply.github.com>
2024-11-12 21:52:47 -0300

    4to intento

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 
-// Configuración de Sequelize
-const sequelize = new Sequelize('agenda_consultorio', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 module.exports = sequelize;
-
-// mi ramita
