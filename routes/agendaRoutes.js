5a6d26175874b931010a505d9d99ae9849e36f28
Angeli04 <113207808+Angeli04@users.noreply.github.com>
2024-11-14 12:32:25 -0300

    proyecto funcionando
    falta afinar bcryptjs

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -7,4 +7,5 @@ router.post('/lista/agendas/:ID',agendaController.actualizarTurnos)
 router.get('/crear',agendaController.renderCrearAgenda)
 router.post('/crear',agendaController.crearAgenda)
 router.get('/agenda/:ID/:Pid',agendaController.listarTurnos)
+router.post('/turnos/actualizarEstado',agendaController.actualizarTurnos)
 module.exports = router;
