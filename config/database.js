a7374cf3ab86b569d3e1a34b9f644fd21fda3c46
Salvador Torres <90728148+Salva080@users.noreply.github.com>
2024-11-13 22:37:00 -0300

    Render

--- config/database.js
+++ config/database.js
@@ -1,10 +1,28 @@
 const { Sequelize } = require('sequelize');
 
+/*
 // Configuración de Sequelize
 const sequelize = new Sequelize('agenda_consultorio', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-});
+});*/
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 module.exports = sequelize;
 
