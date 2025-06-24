c632a075abd0172613048439179c9e70ebbaac6e
Angeli04 <113207808+Angeli04@users.noreply.github.com>
2025-06-24 11:34:51 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,22 +1,21 @@
 const { Sequelize } = require("sequelize");
 
 //Este lo usamos para el servidor online
-/*
-  const sequelize = new Sequelize({
+
+const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  dialect: 'mysql',
-  });
-*/
+  dialect: "mysql",
+});
 
 //Este se usa de forma local
-
+/*
 const sequelize = new Sequelize("agenda_consultorio", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
-
+*/
 module.exports = sequelize;
